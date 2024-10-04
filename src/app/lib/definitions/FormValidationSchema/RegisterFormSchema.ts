4ff8f560a4f6b68e1989f5de127237a5ba0ad381
Mark Trupiano <markt6749@gmail.com>
2024-10-03 23:15:55 -0700

    add zod validation for register form

---
+++ src/app/lib/definitions/FormValidationSchema/RegisterFormSchema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+const RegisterFormSchema = z.object({
+  email: z.string().email({ message: "Invalid email" }).min(2, "Email required"),
+  password: z.string().min(8, "Password must contain at least 8 characters")
+    .refine(str => /[A-Za-z]+/.test(str), "Password must contain an alphabetic character")
+    .refine(str => /[0-9]+/.test(str), "Password must contain a number 0-9")
+    .refine(str => /[#?!@$ %^&*-]+/.test(str), "Password must contain one special character"),
+});
+
+export default RegisterFormSchema;
---
+++ src/app/lib/definitions/FormValidationSchema/index.ts
@@ -0,0 +1,1 @@
+export { default as RegisterFormSchema } from "./RegisterFormSchema";

a1552bde340f50c8f09d98cd6c9fbda937b85b05
Mark Trupiano <markt6749@gmail.com>
2024-10-08 22:48:45 -0700

    simplify 'Home' page with next auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,12 @@
-"use client"
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-import { Button, TextField } from "@mui/material";
-import { FormEvent, useState } from "react";
+export default async function Home() {
+  const session = await auth();
 
-export default function Home() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  if (!session) {
+    redirect("/register");
+  }
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    
-    const response = await fetch('/api/auth/register', {
-      method: "POST",
-      body: {
-        email,
-        password,
-      },
-    });
-
-    console.log(response)
-  };
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <TextField 
-          label="E-mail" 
-          // type="email"
-          value={email} 
-          onChange={e => setEmail(e.target.value)}
-          required
-        />
-        <TextField 
-          label="Password" 
-          type="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          required
-        />
-        <Button type="submit" hidden={true} />
-      </form>
-    </div>
-  );
+  return <div>Hello</div>;
 }

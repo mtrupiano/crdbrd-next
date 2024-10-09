86f87d6df1c20ea57c639ea517b5d3ad5cec5b44
Mark Trupiano <markt6749@gmail.com>
2024-10-08 22:49:01 -0700

    remove min character constraint from email field

--- src/app/lib/definitions/FormValidationSchema/RegisterFormSchema.ts
+++ src/app/lib/definitions/FormValidationSchema/RegisterFormSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 const RegisterFormSchema = z.object({
-  email: z.string().email({ message: "Invalid email" }).min(2, "Email required"),
+  email: z.string().email({ message: "Invalid email" }),
   password: z.string().min(8, "Password must contain at least 8 characters")
     .refine(str => /[A-Za-z]+/.test(str), "Password must contain an alphabetic character")
     .refine(str => /[0-9]+/.test(str), "Password must contain a number 0-9")

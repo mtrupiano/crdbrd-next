6b30ded2db58a0c266f007ac727b8f4a2280f537
Mark Trupiano <markt6749@gmail.com>
2025-02-26 11:31:37 -0800

    add cardsInCollection query

--- src/graphql/queries/collectedCardQueries.ts
+++ src/graphql/queries/collectedCardQueries.ts
@@ -10,6 +10,27 @@ builder.queryField("cards", (t) =>
   }),
 );
 
+builder.queryField("cardsInCollection", (t) =>
+  t.prismaConnection({
+    type: "CollectedCard",
+    cursor: "id",
+    args: {
+      collectionSpaceId: t.arg({
+        type: "ID",
+        description: "CollectionSpace database UUID",
+      }),
+    },
+    resolve: (query, root, args) => {
+      return prisma.collectedCard.findMany({
+        ...query,
+        where: {
+          collectionSpaceId: args.collectionSpaceId,
+        },
+      });
+    },
+  }),
+);
+
 builder.queryField("cardsInLocation", (t) =>
   t.prismaConnection({
     type: "CollectedCard",

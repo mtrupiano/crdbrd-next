f231796f9b6c563a80b0f450c7ca2ad5bf5597b0
Mark Trupiano <markt6749@gmail.com>
2025-02-28 10:25:18 -0800

    update sign out button

--- src/app/SignOutButton.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { Button } from "@mui/material";
-import { signOut } from "next-auth/react";
-
-export default function SignOutButton() {
-  return <Button onClick={signOut}>Sign Out</Button>;
-}

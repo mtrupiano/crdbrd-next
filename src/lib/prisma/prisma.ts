7eacbdbbc5cf9ba9957ec53db726e095a0ab9455
Mark Trupiano <markt6749@gmail.com>
2025-03-23 20:15:24 -0700

    add middleware to filter archived entities from queries

--- src/lib/prisma/prisma.ts
+++ src/lib/prisma/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 let prisma: PrismaClient;
 
@@ -6,12 +6,32 @@ declare global {
   const prisma: PrismaClient;
 }
 
+const filterArchived = Prisma.defineExtension({
+  name: "filterArchived",
+  query: {
+    $allModels: {
+      async $allOperations({ operation, args, query }) {
+        if (
+          operation === "findUnique" ||
+          operation === "findFirst" ||
+          operation === "findMany"
+        ) {
+          args.where = { ...args.where, archivedAt: null };
+          return query(args);
+        }
+        return query(args);
+      },
+    },
+  },
+});
+
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient().$extends(filterArchived);
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient();
+    global.prisma = new PrismaClient().$extends(filterArchived);
   }
   prisma = global.prisma;
 }
-export default prisma as prismaClient;
+
+export default prisma;

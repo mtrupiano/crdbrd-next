ad2bc58c389d57f3f303cafd44cab2f0e586d0e1
Mark Trupiano <markt6749@gmail.com>
2025-04-02 11:33:01 -0700

    update secondary button colors

--- src/app/components/Button/SecondaryButton.tsx
+++ src/app/components/Button/SecondaryButton.tsx
@@ -8,7 +8,7 @@ export default function SecondaryButton({
   return (
     <button
       {...props}
-      className="cursor-pointer text-steel-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-transparent ring-1 ring-steel-blue-800 hover:bg-gray-200 hover:ring-steel-blue-400 focus:ring-2 focus:ring-steel-blue-400 focus:outline-hidden"
+      className="cursor-pointer text-steel-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-transparent ring-1 ring-steel-blue-800 hover:bg-gray-50 hover:ring-steel-blue-500 focus:ring-2 focus:ring-steel-blue-400 focus:outline-hidden"
     />
   );
 }

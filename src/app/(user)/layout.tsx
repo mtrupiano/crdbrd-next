cba82a93bd77d1a1dc218fd12a93c9355f7f247a
Mark Trupiano <markt6749@gmail.com>
2025-04-02 11:29:05 -0700

    add padding to user layout

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -10,5 +10,5 @@ export default async function UserLayout({
   if (!session) {
     redirect("/auth/signin");
   }
-  return <>{children}</>;
+  return <div className="py-4 px-8">{children}</div>;
 }

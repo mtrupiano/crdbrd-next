ff1b205852be7caae79da773d76b682cc63180f5
Mark Trupiano <markt6749@gmail.com>
2025-04-10 10:54:19 -0700

    add avatarwidget to nav bar

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,17 +1,17 @@
 import { auth } from "@/lib/auth";
-import SignOutButton from "./SignOutButton";
+import SignInButton from "./SignInButton";
+import AvatarWidget from "../(user)/components/AvatarWidget";
 
 export default async function NavBar() {
   const session = await auth();
   return (
-    <nav className="bg-blue-950 w-full h-28 flex items-center justify-between px-8">
+    <nav className="bg-steel-blue-900 w-full h-20 flex items-center justify-between px-8">
       <a href="/">
         <span className="text-2xl text-white">Home</span>
       </a>
-      {session && (
-        <span className="text-white">Hello {session?.user?.email}</span>
-      )}
-      {session && <SignOutButton />}
+
+      {session && <AvatarWidget session={session} />}
+      {!session && <SignInButton />}
     </nav>
   );
 }
